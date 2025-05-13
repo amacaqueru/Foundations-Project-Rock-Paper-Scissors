19ec9c84bdbbfd1e20e46d66bd968f0174cf7cf1
amacaqueru <nmreinosoc@gmail.com>
2025-05-13 13:29:20 -0500

    updated with buttons

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ function getComputerChoice() {
     return "rock";
   } else if (choice == 1) {
     papel++;
-    return "paper";
+    return "papers";
   } else {
     tijeras++;
     return "scissors";
@@ -48,8 +48,9 @@ function getComputerChoice() {
 // }
 // }
 
-// let humanScore = 0;
-// let computerScore = 0;
+let humanScore = 0;
+let computerScore = 0;
+let partida = 0;
 
 function playRound(humanChoice, computerChoice) {
   console.log(`Human Choice: ${humanChoice}`);
@@ -59,60 +60,69 @@ function playRound(humanChoice, computerChoice) {
     if (computerChoice == "rock") {
       console.log("Empates");
       return 0;
-    } else if (computerChoice == "paper") {
+    } else if (computerChoice == "papers") {
+      console.log("paper wins");
       return 2;
     } else {
+      console.log("rock wins");
       return 1;
     }
-  } else if (humanChoice == "paper") {
+  } else if (humanChoice == "papers") {
     if (computerChoice == "rock") {
+      console.log("paper wins");
       return 1;
-    } else if (computerChoice == "paper") {
+    } else if (computerChoice == "papers") {
       console.log("Empates");
       return 0;
     } else {
+      console.log("scissors win");
       return 2;
     }
   } else if (humanChoice == "scissors") {
     if (computerChoice == "rock") {
+      console.log("rock wins");
       return 2;
-    } else if (computerChoice == "paper") {
+    } else if (computerChoice == "papers") {
+      console.log("scissor wins");
       return 1;
     } else {
+      console.log("empates");
       return 0;
     }
   }
 }
 
-function playGame() {
-  let humanScore = 0;
-  let computerScore = 0;
-  let continuar = true;
+function playGame(buttonChoice) {
+  console.log(buttonChoice);
+  //   let humanScore = 0;
+  //   let computerScore = 0;
+  //   let continuar = true;
 
-  let partida = 0;
+  console.log(`Partida: ${partida}`);
+  let result = playRound(buttonChoice, getComputerChoice());
+  // playRound(getHumanChoice(), getComputerChoice());
+  if (result == 1) {
+    humanScore++;
+  } else if (result == 2) {
+    computerScore++;
+  }
 
-  while (continuar) {
-    partida++;
-    console.log(`Partida: ${partida}`);
-    let result = playRound(getHumanChoice(), getComputerChoice());
-    // playRound(getHumanChoice(), getComputerChoice());
-    if (result == 1) {
-      humanScore++;
-    } else if (result == 2) {
-      computerScore++;
-    }
-
-    if (humanScore >= 3) {
-      console.log("Humano gana");
-      console.log("Puntuacion final: ");
-      console.log(`Humano: ${humanScore}, Computadora: ${computerScore}`);
-      continuar = false;
-    } else if (computerScore >= 3) {
-      console.log("Computadora gana");
-      console.log("Puntuacion final: ");
-      console.log(`Humano: ${humanScore}, Computadora: ${computerScore}`);
-      continuar = false;
-    }
+  if (humanScore >= 5) {
+    console.log("Humano gana");
+    console.log("Puntuacion final: ");
+    console.log(`Humano: ${humanScore}, Computadora: ${computerScore}`);
+    humanScore = 0;
+    computerScore = 0;
+    partida = 0;
+    console.log("-----Reset-----\n\n\n");
+  } else if (computerScore >= 5) {
+    console.log("Computadora gana");
+    console.log("Puntuacion final: ");
+    console.log(`Humano: ${humanScore}, Computadora: ${computerScore}`);
+    humanScore = 0;
+    computerScore = 0;
+    partida = 0;
+    console.log("-----Reset-----\n\n\n");
   }
 }
 
@@ -120,12 +130,27 @@ let rockBtn = document.getElementById("rock");
 let papersBtn = document.getElementById("papers");
 let scissorsBtn = document.getElementById("scissors");
 
-// console.log(rockBtn);
-// console.log(papersBtn);
-// console.log(scissorsBtn);
+rockBtn.addEventListener("click", variableRoca);
+papersBtn.addEventListener("click", variablePapers);
+scissorsBtn.addEventListener("click", variableScissors);
 
+function variableRoca() {
+  partida++;
+  playGame(rockBtn.innerText.toLowerCase());
+}
+
+function variablePapers() {
+  partida++;
+  playGame(papersBtn.innerText.toLowerCase());
+}
+
+function variableScissors() {
+  partida++;
+  playGame(scissorsBtn.innerText.toLowerCase());
+}
+
+// console.log(rockBtn.innerText.toLowerCase());
+// console.log(papersBtn.innerText.toLowerCase());
+// console.log(scissorsBtn.innerText.toLowerCase());
 
 // playGame();
-
-
-
